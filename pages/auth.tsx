a9bf03baa2dd55f5e79b7ff4d9f61d56742fd2c8
harshxraj <harshraj0588@gmail.com>
2023-10-14 19:18:39 +0530

    auth page in progress 2

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,11 @@
 import Input from "@/components/Input";
+import { useState } from "react";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-fixed bg-center bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -9,12 +14,43 @@ const Auth = () => {
         </nav>
         {/* inputs */}
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-wd rounded-md w-full">
+          <div className="bg-black bg-opacity-70 px-16 lg:py-16 md:py-4 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             {/* input boxes */}
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+
             <div className="flex flex-col gap-4">
-              <Input />
+              <Input
+                label="Username"
+                onChange={(e: any) => setName(e.target.value)}
+                id="name"
+                type="text"
+                value={name}
+              />
+              <Input
+                label="Email"
+                onChange={(e: any) => setEmail(e.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+              <Input
+                label="Password"
+                onChange={(e: any) => setPassword(e.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
             </div>
+            {/* button */}
+            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-8 hover:bg-red-700 transition">
+              Login
+            </button>
+            <p className="text-neutral-500 mt-12">
+              First time using Netflix?
+              <span className="text-white ml-2 hover:underline cursor-pointer">
+                Create an account.
+              </span>
+            </p>
           </div>
         </div>
       </div>
