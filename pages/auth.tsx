f02bdff1912807e1306332ac1821b9a94ec209b3
harshxraj <harshraj0588@gmail.com>
2023-12-11 13:50:26 +0530

    authentication using email and password done #2

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,74 @@
 import Input from "@/components/Input";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import toast, { Toaster } from "react-hot-toast";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      const result = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      console.log(result);
+      // Check if the authentication was successful
+      if (result?.ok) {
+        // toast.success("Logged in!");
+        router.push("/");
+      } else {
+        // Handle unsuccessful authentication
+        toast.error(`${result?.error}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      var emailRegex = /^[A-Za-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+      if (!emailRegex.test(email)) {
+        alert("Enter valid email");
+        return;
+      }
+
+      if (email === "" || password == "" || name == "") {
+        alert("Invalid");
+        return;
+      }
+
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-fixed bg-center bg-cover">
@@ -16,16 +80,20 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 lg:py-16 md:py-4 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             {/* input boxes */}
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
 
             <div className="flex flex-col gap-4">
-              <Input
-                label="Username"
-                onChange={(e: any) => setName(e.target.value)}
-                id="name"
-                type="text"
-                value={name}
-              />
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(e: any) => setName(e.target.value)}
+                  id="name"
+                  type="text"
+                  value={name}
+                />
+              )}
               <Input
                 label="Email"
                 onChange={(e: any) => setEmail(e.target.value)}
@@ -42,18 +110,27 @@ const Auth = () => {
               />
             </div>
             {/* button */}
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-8 hover:bg-red-700 transition">
-              Login
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-8 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
-            <p className="text-neutral-500 mt-12">
-              First time using Netflix?
-              <span className="text-white ml-2 hover:underline cursor-pointer">
-                Create an account.
+            <p className="text-neutral-500 mt-10">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-2 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
               </span>
             </p>
           </div>
         </div>
       </div>
+      <Toaster />
     </div>
   );
 };
