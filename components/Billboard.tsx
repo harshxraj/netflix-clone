565657e3d04d856610aa1f389b522db2b90e17ec
harshxraj <harshraj0588@gmail.com>
2023-12-12 19:06:02 +0530

    adding billboard

---
+++ components/Billboard.tsx
@@ -0,0 +1,8 @@
+import useBillboard from "@/hooks/useBillboard";
+import React from "react";
+
+const Billboard = () => {
+  const { data } = useBillboard();
+  return <div></div>;
+};
+export default Billboard;
